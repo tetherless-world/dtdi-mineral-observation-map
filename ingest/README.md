3d6cc8db144a33c1cebad7be2da72ba0503bdc72
zhonghao1014 <zhonghao1014@gmail.com>
2016-01-26 02:04:25 -0500

    parsing script completed, which generates an uploadable bulk_file; browser updated and is working with the new data structure, need further work but basically working; README updated with some notes; mapping not updated yet, still using the auto-generated mapping through uploading

--- ingest/README.md
+++ ingest/README.md
@@ -1,3 +1,76 @@
+***This is temporarily a scratch pad and will be a formatted README as it goes on***
+
+***Instructions:
+
+Generate bulk-file:
+
+``python3 parseOriginalData.py''
+
+Upload bulk-file:
+
+``curl -XPOST 'localhost:9200/_bulk' --data-binary @bulk_file''
+
+====================================================================
+
+
+
+1) Go to http://rruff.info/ima/ and log in.
+
+2) Select "IMA Approved Minerals Only"
+
+3) Export Minerals. Export Options: (Below are the useful ones, almost is everything)
+
+  - Mineral Name (plain)
+  - RRUFF Chemistry (plain)
+  - RRUFF Chemistry (concise)
+  - IMA Chesmistry (plain)
+  - IMA Chesmistry (concise)
+  - Chemical Elements
+  - RRUFF IDs
+  - IMA Number
+  - Database ID***?
+  - Country 
+  - Structural Groupname
+  - Fleischers Groupname
+  - IMA Status
+  - Status Notes
+  - Year First Published
+
+Mindat link: www.mindat.org/search.php?name=Abelsonite
+or: http://www.mindat.org/show.php?name=Abelsonite
+
+4) Export Evolution Records for each minerals
+First go to http://rruff.info/mineral_list/locality.php?mineral_name=xxxxxxxxxxx(the mineral name, not case-sensitive)
+Click "Show GPS"
+Click "Export Table"
+Save the data from the 3rd column to an csv file. ()
+
+Locality IDs:
+http://rruff.info/mineral_list/locality.php?mindat_id=20265
+http://www.mindat.org/loc-20265.html
+
+
+
+
+Does these matter in the future
+    This age satisfies at least one of these three conditions:
+1) Mindat claims that this mineral occurs at this locality.
+2) The locality containing the mineral has an age directly assigned to it.
+3) This locality is displaying an age from a non-child locality.
+    This age does not satisfy any of the above conditions.
+
+ G  This mineral is directly dated.
+ B  This mineral is explicitely specified as having an age.
+ Y  This mineral is using an age from a mineralization period.
+ O  This mineral is using an age calculated from all data at the locality.
+ R  The age displayed for this mineral is coming from a different, non-child locality.
+  This mineral is not associated with an age.
+
+
+
+
+
+
 *** Some scripts for convenience. Just as a scratchpad for now ***
 
 
@@ -6,36 +79,7 @@ curl -XPUT 'localhost:9200/dtdi/mineral-observation/_mapping?pretty' --data-bina
 
 curl -XPOST 'localhost:9200/_bulk' --data-binary @[out]
 
-
-
 curl -XGET 'localhost:9200/dtdi/mineral-observation/_mapping?pretty'
-
-
-
-
-POST /dtdi/mineral-observation/
-{  
-  "locality":[  
-    {  
-      "country":"Canada",
-      "region":"Québec, Canada",
-      "mine":"Poudrette quarry (Demix quarry; Uni-Mix quarry; Desourdy quarry; Carrière Mont Saint-Hilaire), Mont Saint-Hilaire, La Vallée-du-Richelieu RCM, Montérégie, Québec, Canada",
-      "coordinates":{  
-        "lat":"45.56277778",
-        "lon":"-73.14166667"
-      },
-      "mindat":"http://www.mindat.org/loc-599.html"
-    }
-  ],
-  "description":"Named after the Abenaki, the indigenous people who inhabited the area around Mont Saint-Hilaire in southern Québec, prior to the arrival of European settlers.",
-  "mindat":"http://www.mindat.org/min-2.html",
-  "mineral":{  
-    "name":"Abenakiite-(Ce)",
-    "rruffChemistryConcise":"Na_26_Ce^3+^_6_(SiO_3_)_6_(PO_4_)_6_(CO_3_)_6_(S^4+^O_2_)O",
-    "rruffChemistryHTML":"Na<sub>26</sub>Ce<sup>3+</sup><sub>6</sub>(SiO<sub>3</sub>)<sub>6</sub>(PO<sub>4</sub>)<sub>6</sub>(CO<sub>3</sub>)<sub>6</sub>(S<sup>4+</sup>O<sub>2</sub>)O",
-    "elements":"Na Ce Si O P C S"
-  }
-}
 
 DELETE dtdi/mineral-observation
 
--- ingest/example/parseOriginalData.py
+++ ingest/example/parseOriginalData.py
@@ -1,9 +1,10 @@
 import requests
 import csv, json
 
+
 def printJSONString(x):
-	y = json.loads(x)
 	print(json.dumps(json.loads(x), indent=2, sort_keys=True))
+
 
 def printJSONPretty(x):
 	print(json.dumps(x, indent=2, sort_keys=True))
@@ -11,45 +12,68 @@ def printJSONPretty(x):
 
 es_index_line = '{"index":{"_type":"mineral-observation","_index":"dtdi"}}\n'
 
-mineral_observations_file = open("mineral_observations.csv", 'w+')
-bulk_file = open("bulk", "w+")
+
+# Open bulk_file to write
+bulk_file = open("bulk_file", "w+")
+
 
 mineral_list_reader = csv.DictReader(open("RRUFF_Export.csv", 'r', encoding='utf-8', errors='ignore'))
-mineral_list_json_string = json.dumps([x for x in mineral_list_reader ], indent=2, sort_keys=True)
-# print(mineral_list_json_string[0:750])
-mineral_list_json = json.loads(mineral_list_json_string)
-# print([x['Mineral Name'] for x in mineral_list_json[0:10]])
+mineral_list_JSON_string = json.dumps([x for x in mineral_list_reader ], indent=2, sort_keys=True)
+mineral_list_JSON = json.loads(mineral_list_JSON_string)
 
-mineral_observations = []
-for mineral_json in mineral_list_json[0:10]:
-	mineral_json["mineral-name-plain"] = mineral_json["Mineral Name (plain)"]
-	# Create Chemistry Elements List
-	mineral_json["Chemistry Elements List"] = mineral_json["Chemistry Elements"].strip().split(" ")
-	# Create Mindat-based URI for the mineral
-	mineral_json["Mineral Mindat"] = requests.get("http://www.mindat.org/search.php?name=" + mineral_json["Mineral Name (plain)"]).url
+
+# mineral_observations = []
+for mineral_json in mineral_list_JSON[0:10]:
+	mineral = json.loads("{}")
+	mineral["mineral-name-plain"] = mineral_json["Mineral Name (plain)"]
+	mineral["chemistry-elements-list"] = mineral_json["Chemistry Elements"].strip().split(" ")
+	mineral["mineral-mindat-url"] = requests.get("http://www.mindat.org/search.php?name=" + mineral_json["Mineral Name (plain)"]).url
+	mineral["country-of-type-locality"] = mineral_json["Country of Type Locality"]	
+	mineral["fleischers-groupname"] = mineral_json["Fleischers Groupname"]
+	mineral["ima-chesmistry-html"] = mineral_json["IMA Chemistry (HTML)"]
+	mineral["ima-chesmistry-concise"] = mineral_json["IMA Chemistry (concise)"]
+	mineral["ima-number"] = mineral_json["IMA Number"]
+	mineral["rruff-chemistry-html"] = mineral_json["RRUFF Chemistry (HTML)"]
+	mineral["rruff-chemistry-concise"] = mineral_json["RRUFF Chemistry (concise)"]
+	mineral["year-first-published"] = mineral_json["Year First Published"]
+
 	# Now we have mineral information ready.
 	# We go through each observation from the locality list, and generate mineral observation records
 	locality_list_reader = csv.DictReader(open(mineral_json["Mineral Name (plain)"] + ".csv", 'r', encoding='utf-8', errors='ignore'))
 	locality_list_json_string = json.dumps([x for x in locality_list_reader ], indent=2, sort_keys=True)
 	locality_list_json = json.loads(locality_list_json_string)
 	for locality_json in locality_list_json:
-		# Adding some locality properties
-		locality_json["Locality Mindat"] = "http://www.mindat.org/loc-" + locality_json["MinDat ID"] + ".html"
-		locality_json["Lat"] = int(locality_json["Lat Deg"]) + int(locality_json["Lat Min"])/60 + int(locality_json["Lat Sec"])/3600
-		locality_json["Lon"] = int(locality_json["Lon Deg"]) + int(locality_json["Lon Min"])/60 + int(locality_json["Lon Sec"])/3600
+		locality = json.loads("{}")
+		locality["locality-label"] 	= locality_json["Locality containing Mineral"]
+		locality["locality-mindat-id"]  = locality_json["MinDat ID"]
+		locality["locality-mindat-url"] = "http://www.mindat.org/loc-" + locality_json["MinDat ID"] + ".html"
 		_country_region = locality_json["Locality containing Mineral"].strip().split(",")
-		locality_json["Country"] = _country_region[len(_country_region)-1].strip()		
-		locality_json["Region"] =  _country_region[len(_country_region)-2].strip() + ", " + _country_region[len(_country_region)-1].strip()
+		locality["country"] = _country_region[len(_country_region)-1].strip()		
+		locality["region"] =  _country_region[len(_country_region)-2].strip() + ", " + _country_region[len(_country_region)-1].strip()
+		locality["lat"] = int(locality_json["Lat Deg"]) + int(locality_json["Lat Min"])/60 + int(locality_json["Lat Sec"])/3600
+		locality["lon"] = int(locality_json["Lon Deg"]) + int(locality_json["Lon Min"])/60 + int(locality_json["Lon Sec"])/3600
+		locality["lat-deg"] = locality_json["Lat Deg"]
+		locality["lat-min"] = locality_json["Lat Min"]
+		locality["lat-sec"] = locality_json["Lat Sec"]
+		locality["lon-deg"] = locality_json["Lon Deg"]
+		locality["lon-min"] = locality_json["Lon Min"]
+		locality["lon-sec"] = locality_json["Lon Sec"]
+		locality["decimal-degree"] = locality_json["Decimal Degree"]
+		locality["max-age"] = locality_json["Max Age (Ma)"]
+		locality["min-age"] = locality_json["Min Age (Ma)"]
+		locality["dated-locality-label"] = locality_json["Dated Locality (Max Age)"]
+		locality["dated-locality-mindat-id"]  = locality_json["Dated Locality ID"]
+		locality["dated-locality-mindat-url"] = "http://www.mindat.org/loc-" + locality_json["Dated Locality ID"] + ".html"
 		# Put mineral and locality info into mineral_observation
 		mineral_observation = json.loads('{"mineral":{},"locality":{}}')
-		mineral_observation["mineral"] = mineral_json
-		mineral_observation["locality"] = locality_json
+		mineral_observation["mineral"] = mineral
+		mineral_observation["locality"] = locality
 		# Next, append to min-obs list and complete one entry, and also add item to bulk
 		bulk_file.write(es_index_line)
 		bulk_file.write(json.dumps(mineral_observation) + "\n")
-		mineral_observations.append(mineral_observation)
+		# mineral_observations.append(mineral_observation)
 
 
-print(json.dumps(mineral_observations[0], indent=2, sort_keys=True))
-print(len(mineral_observations))
+# print(json.dumps(mineral_observations[2], indent=2, sort_keys=True))
+# print(len(mineral_observations))
 
